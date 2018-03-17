fd2763d4d020fe8ebd39b891fa97a5227bc7bee5
stevepichardo <stevepichardo@outlook.com>
2018-03-16 21:01:11 -0400

    fully working

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -20,8 +20,8 @@ namespace TechJobs.Controllers
         // The detail display for a given Job at URLs like /Job?id=17
         public IActionResult Index(int id)
         {
-            JobData job = JobData.GetInstance();
-            var singleJob = job.Find(id);
+            
+            var singleJob = jobData.Find(id);
             return View(singleJob);
         }
 
@@ -34,9 +34,21 @@ namespace TechJobs.Controllers
         [HttpPost]
         public IActionResult New(NewJobViewModel newJobViewModel)
         {
-            // TODO #6 - Validate the ViewModel and if valid, create a 
-            // new Job and add it to the JobData data store. Then
-            // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID),
+                };
+
+                jobData.Jobs.Add(newJob);
+                return RedirectToAction("Index", new { id = newJob.ID });
+
+            }
 
             return View(newJobViewModel);
         }
---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public abstract class BaseViewModel
+    {
+        public JobFieldType Column { get; set; }
+        public List<JobFieldType> Columns { get; set; }
+        public string Title { get; set; } = "";
+
+        protected BaseViewModel()
+        {
+
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+
+        }
+    }
+}
